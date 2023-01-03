d2c5a532f3f9890326cbfe1b7e7e9d030957ce04
Rwakasiisi Edwin <erwakasiisi@gmail.com>
2023-01-03 21:51:24 +0300

    mvp

--- dashboard/index.php
+++ dashboard/index.php
@@ -8,10 +8,8 @@
     <title>Cjay Admin</title>
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
     <!-- bootstrap CSS File -->
     <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
 </head>
 <body class="bg-black">
     <div class="container-fluid">
@@ -27,9 +25,11 @@
                         <img src="../assets/images/team/portrait.jpg" class="rounded-circle img-fluid" alt="" width="40px">
                     </div>
                     <div class="username">
-
+                        <h5>Edwin Codes</h5>
                     </div>
-                    <div class="user-title"></div>
+                    <div class="user-title">
+                        <h5>Super Admin</h5>
+                    </div>
                 </li>
             </ul>
 
